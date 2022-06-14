5ed24ef2c8d1ff0d7c5201572de35c22521d939d
ChyoRi <chyori_@naver.com>
2022-06-15 00:31:18 +0900

    last commit

--- js/index.js
+++ js/index.js
@@ -7,11 +7,17 @@ console.log(mainMenu);
 function clickHandler(e) {
     const btn = this.children[0];
     const subMenuWrap = this.nextElementSibling;
-    console.log(btn);
-    console.log(e);
-    console.log(subMenuWrap);
+    // console.log(btn);
+    // console.log(e);
+    // console.log(subMenuWrap);
     btn.classList.toggle('active_btn');
+    // if (subMenuWrap)
     subMenuWrap.classList.toggle('active');
+    if (subMenuWrap.style.maxHeight) {
+        subMenuWrap.style.maxHeight = null;
+    } else {
+        subMenuWrap.style.maxHeight = subMenuWrap.scrollHeight + "px";
+    }
 }
 
 
